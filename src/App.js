75cbe3947caebb48bac102ffcccdc7a0fd07834f
KhadijahAli14 <kady.ab.2020@gmail.com>
2022-08-24 11:54:51 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [pokemons, setPokemon] = useState({
+      results : []
+    });
+
+ // fetch pokemons from API via  Axios
+  const getPokemon = () => {
+        axios.get('https://pokeapi.co/api/v2/pokemon/').then(response => { setPokemon(response.data) })// resolve response and setPeople as response.results
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="container d-flex align-items-center flex-column">
+      <button className="btn btn-outline-info m-3 " onClick={getPokemon}>Fetch Pokemon</button>
+       <h4 className="">Pokémon</h4>
+     
+        {pokemons.results && pokemons.results.map((pokemon, url) => { 
+          return (
+             <ul className='list-group' key={pokemon.url}> 
+              <li className="list-group-items list-group-item-action m-1"> {pokemon.name} - { pokemon.url}</li>
+            </ul>
+            
+          )
+        })}
+    
     </div>
   );
 }
